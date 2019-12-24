0bbcd7b93b6a00eb726109755ec11133e098f74a
Patrick Shaw <sixtey7@gmail.com>
2019-12-23 23:03:04 -0500

    start building out the frontend

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-app dark style = "margin-top: 0px">
+        <v-card height="100%">
+            <v-content>
+                <v-container>
+                    Hello World
+                </v-container>
+            </v-content>
+        </v-card>   
+    </v-app>
+</template>
+<script>
+export default {
+    name: 'app',
+    components: {
+    },
+}
+</script>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueLogger from 'vuejs-logger'
+import vuetify from '@/plugins/vuetify' // path to vuetify export
+import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
+
+Vue.config.productionTip = false
+
+const isProduction = process.env.NODE_ENV === 'production';
+const options = {
+    isEnabled: true,
+    logLevel : isProduction ? 'error' : 'debug',
+    stringifyArguements: false,
+    showLogLevel: true,
+    showMethodName: true,
+    separator: '|',
+    showConsoleColors: true
+};
+
+Vue.use(VueLogger, options);
+
+new Vue({
+    vuetify,
+    render: h => h(App)
+}).$mount('#app')
