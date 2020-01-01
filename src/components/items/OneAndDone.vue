a0d9d7dee23a8fd72ee8eaa653e1b503181e98cd
Patrick Shaw <sixtey7@gmail.com>
2020-01-01 10:09:46 -0500

    start off 2020 with some comments!

--- src/components/items/OneAndDone.vue
+++ src/components/items/OneAndDone.vue
@@ -1,28 +1,36 @@
 <template>
-    <v-list-item>
-        <v-list-item-action>
-            <v-checkbox
-                v-model="item.current_value"
-                color="primary"
-                @change="one_and_done_toggle()"
-            ></v-checkbox>
-        </v-list-item-action>
-        <v-list-item-content v-text="item.name">
-        </v-list-item-content>
-    </v-list-item>
+  <v-list-item>
+    <v-list-item-action>
+      <v-checkbox
+        v-model="item.current_value"
+        color="primary"
+        @change="one_and_done_toggle()"
+      ></v-checkbox>
+    </v-list-item-action>
+    <v-list-item-content v-text="item.name"> </v-list-item-content>
+  </v-list-item>
 </template>
 <script>
+/**
+ * @vue-prop {Object} item - the item being displayed in the vue
+ * @vue-prop {Object} logger - The logger instance to be used
+ * @vue-event {Object} changed - Emit the updated item when changed
+ */
 export default {
-    name: "OneAndDone",
-    props: {
-        item: Object,
-        logger: Object
-    },
-    methods: {
-        one_and_done_toggle: function() {
-            this.logger.debug('item is currently: ' + JSON.stringify(this.item));
-            this.$emit('changed', this.item);
-        }
+  name: "OneAndDone",
+  props: {
+    item: Object,
+    logger: Object
+  },
+  methods: {
+    /**
+     * Called when the user clicks on the checkbox, alerts the parent component
+     * @vue-event {Object} changed - Emits the updated item
+     */
+    one_and_done_toggle: function() {
+      this.logger.debug("item is currently: " + JSON.stringify(this.item));
+      this.$emit("changed", this.item);
     }
-}
+  }
+};
 </script>
--- src/components/list/ListView.vue
+++ src/components/list/ListView.vue
@@ -1,63 +1,66 @@
 <template>
-      <v-card
-        max-width="600"
-        class="mx-auto">
-        <div id = "category-item-list-div">
-            <v-list-group
-                v-for="category in categoryArray"
-                :key = "category.id"
-            >
-                <template v-slot:activator>
-                    <v-list-item-title>{{category.name}}</v-list-item-title>
-                </template>
-                <div
-                    v-for="item in itemMap[category.id]"
-                    :key="item.id">
-                    <OneAndDone
-                        :item="item" 
-                        :logger="logger"
-                        @changed="one_and_done_toggle"/>
-                </div>
-                
-            </v-list-group>
+  <v-card max-width="600" class="mx-auto">
+    <div id="category-item-list-div">
+      <v-list-group v-for="category in categoryArray" :key="category.id">
+        <template v-slot:activator>
+          <v-list-item-title>{{category.name}}</v-list-item-title>
+        </template>
+        <div v-for="item in itemMap[category.id]" :key="item.id">
+          <OneAndDone :item="item" :logger="logger" @changed="one_and_done_toggle" />
         </div>
-      </v-card>
+      </v-list-group>
+    </div>
+  </v-card>
 </template>
 <script>
-import CategoryModel from '../../model/CategoryModel';
-import ItemHelper from '../../utils/ItemHelper';
-import OneAndDone from '../items/OneAndDone';
-export default {
-    name: 'ListView',
-    components: {
-        OneAndDone
-    },
-    data() {
-        return {
-            itemHelper: new ItemHelper(this.logger),
-            itemMap: new Map()
-        }
-    },
-    props: {
-        logger: Object,
-        categoryModel: CategoryModel,
-        categoryArray: Array,
-        itemArray: Array
-    },
-    methods: {
-        one_and_done_toggle: function(updatedItem) {
-            this.logger.debug('got a one and done change event: ' + JSON.stringify(updatedItem));
-            this.$emit('updatedItem', updatedItem);
-        }
-        
-    },
-    watch: {
-        itemArray: function(val) {
-            this.logger.debug('Item array changed, new length is: ' + val.length);
-            this.itemMap = this.itemHelper.createMapByCategory(val);
-            this.logger.debug('Got the built map: ' + JSON.stringify(this.itemMap));
-        }
-    }
+/**
+ * Vue component forming the top-level visual of the display
+ * @vue-prop {Object} logger - the Logger to be used for the component
+ * @vue-prop {Object} categoryModel - The model object for categories
+ * @vue-prop {Object} categroyArray - The array of all categories 
+ * @vue-prop {Array} itemArray - The array of all items
+ * @vue-event {Object} updatedItem - Emit the updated item when changed
 
-}
+ */
+import CategoryModel from "../../model/CategoryModel";
+import ItemHelper from "../../utils/ItemHelper";
+import OneAndDone from "../items/OneAndDone";
+export default {
+  name: "ListView",
+  components: {
+    OneAndDone
+  },
+  data() {
+    return {
+      itemHelper: new ItemHelper(this.logger),
+      itemMap: new Map()
+    };
+  },
+  props: {
+    logger: Object,
+    categoryModel: CategoryModel,
+    categoryArray: Array,
+    itemArray: Array
+  },
+  methods: {
+      /**
+       * Called when a child component emits a changed event
+       * @prop {Object} updatedItem - the item that was updated
+       * @vue-event {Object} updatedItem - emits back out the received updated item to the parent component
+       */
+    one_and_done_toggle: function(updatedItem) {
+      this.logger.debug(
+        "got a one and done change event: " + JSON.stringify(updatedItem)
+      );
+      this.$emit("updatedItem", updatedItem);
+    }
+  },
+  watch: {
+    itemArray: function(val) {
+      this.logger.debug("Item array changed, new length is: " + val.length);
+      this.itemMap = this.itemHelper.createMapByCategory(val);
+      this.logger.debug("Got the built map: " + JSON.stringify(this.itemMap));
+    }
+  }
+};
 </script>
