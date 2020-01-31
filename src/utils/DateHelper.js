09d7b9217dbdba17ead1f0a29ee0a53525c7e903
Patrick Shaw <sixtey7@gmail.com>
2020-01-30 19:08:05 -0500

    comments are good

--- src/utils/DateHelper.js
+++ src/utils/DateHelper.js
@@ -1,5 +1,8 @@
 import moment from 'moment';
 
+/**
+ * Helper class used for handling date objects
+ */
 class DateHelper {
     _logger
 
@@ -7,10 +10,18 @@ class DateHelper {
         this._logger = logger;
     }
 
+    /**
+     * Takes in a date from the backend and returns a javascript date object
+     * @param {String} dateToMassage The date to be massaged in backend format
+     */
     massageDateForFrontend(dateToMassage) {
         return moment(dateToMassage.slice(0, -4), 'ddd, DD MMM YYYY HH:mm:ss');
     }
 
+    /**
+     * Returns a string representation of the date for display to a user
+     * @param {Object} dateObj Javascript date object 
+     */
     createDisplayDate(dateObj) {
         return dateObj.format('MMM DD, YYYY');
     }
--- src/utils/EventHelper.js
+++ src/utils/EventHelper.js
@@ -1,5 +1,8 @@
 import DateHelper from './DateHelper';
 
+/**
+ * Class used to facilitate working with events within the frontend
+ */
 class EventHelper {
     _logger;
     _dateHelper;
@@ -9,6 +12,10 @@ class EventHelper {
         this._dateHelper = new DateHelper(logger);
     }
 
+    /**
+     * Massages the event array returned from the backend for easy handling by the frontend
+     * @param {Array} eventArray The array of events to be massaged 
+     */
     massageEvents(eventArray) {
         this._logger.debug('Event array before massaging: ' + JSON.stringify(eventArray));
 
